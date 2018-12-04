e103074d3dc11f69892efba8bb05a6e2494a2a88
iMac <iMac@iMac-iMac.local>
2018-12-04 22:56:14 +0300

    contact search app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CONTACTS from "./bd.js";
 
-class App extends Component {
+class Contact extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <li className="contact">
+        <img
+          className="contact-image"
+          src={this.props.image}
+          width="60px"
+          height="60px"
+        />
+        <div className="contact-info">
+          <div className="contact-name">{this.props.name}</div>
+          <div className="contact-number">{this.props.phoneNumber}</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+class ContactList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayedContacts: CONTACTS
+    };
+  }
+  handleSearch(event) {
+    var searchQuery = event.target.value.toLowerCase();
+    var displayedContacts = CONTACTS.filter(function(el) {
+      var searchValue = el.name.toLowerCase();
+      return searchValue.indexOf(searchQuery) !== -1;
+    });
+    this.setState({ displayedContacts: displayedContacts });
+  }
+  render() {
+    return (
+      <div className="contacts">
+        <input
+          type="text"
+          className="search-field"
+          onChange={this.handleSearch.bind(this)}
+        />
+        <ul className="contacts-list">
+          {this.state.displayedContacts.map(function(el) {
+            return (
+              <Contact
+                key={el.id}
+                name={el.name}
+                phoneNumber={el.phoneNumber}
+                image={el.image}
+              />
+            );
+          })}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+export default ContactList;
---
+++ src/bd.js
@@ -0,0 +1,39 @@
+var CONTACTS = [
+  {
+    id: 1,
+    name: "Darth Vader",
+    phoneNumber: "+250966666666",
+    image:
+      "https://wallpapertag.com/wallpaper/middle/8/b/9/227581-download-darth-vader-background-1920x1080-ipad.jpg"
+  },
+  {
+    id: 2,
+    name: "Princess Leia",
+    phoneNumber: "+250955555454",
+    image:
+      "http://mir-kvestov.com.ua/uploads/quests/7322/large/kvestmaniya_zvyozdnye_voiny_printsessa_leya_v_poiskah_karty_photo1.jpg?1504257151"
+  },
+  {
+    id: 3,
+    name: "Luke Skywalker",
+    phoneNumber: "+250987878878",
+    image:
+      "http://vignette.wikia.nocookie.net/battlefront/images/d/de/Battlefront_%D0%BB%D1%8E%D0%BA_%D1%81%D0%BA%D0%B0%D0%B9%D1%83%D0%BE%D0%BA%D0%B5%D1%80.jpg/revision/latest?cb=20151111195216&path-prefix=ru"
+  },
+  {
+    id: 4,
+    name: "Chewbacca",
+    phoneNumber: "+250945678986",
+    image:
+      "https://ic.pics.livejournal.com/tab1971/20249776/795417/795417_900.png"
+  },
+  {
+    id: 5,
+    name: "R2D2",
+    phoneNumber: "+250961111111",
+    image:
+      "http://images.esellerpro.com/2243/I/178/55/decor-R2D2-money-box%20(1).JPG"
+  }
+];
+
+export default CONTACTS;
